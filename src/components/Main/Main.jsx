e7c39838bbeb222bede55ab06b80ca5421afd75c
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-21 08:01:49 +0600

    basic skills page created and added it in the Main.jsx file

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar/Navbar";
 import Banner from "../Banner/Banner";
+import Skills from "../Skills/Skills";
 
 const Main = () => {
     return (
         <div className="font-primary text-white max-w-7xl mx-auto">
             <Navbar></Navbar>
             <Banner></Banner>
+            <Skills></Skills>
             <Outlet></Outlet>
         </div>
     );
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,10 @@
+
+const Skills = () => {
+    return (
+        <>
+         <h2>This is skills page</h2>   
+        </>
+    );
+};
+
+export default Skills;
