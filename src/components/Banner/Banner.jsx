2f47ce27b7a31da0b4ac1b58248bae7777a3a99c
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-21 06:30:48 +0600

    added image and texts in the banner section and gradiant also

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,11 @@
+import './banner.css'
 
 const Banner = () => {
     return (
         <>
             {/* div for hold everything in this page */}
-            <div>
+            <div className="w-full md:flex justify-between items-center h-[80vh]">
                 {/* extra two section for hold two section texts and picture  */}
-
 
                 <section>
                     {/* texts about me */}
@@ -14,11 +14,10 @@ const Banner = () => {
                     <h2>A full stact developer</h2>
                     {/* section to hold my social linkd */}
                     <section>
-
                     </section>
                 </section>
-                <section>
-
+                <section className="rounded-xl bg-gradient-to-r from-[#2E2E2E] via-[#444444] to-[#919191]">
+                    <img className="w-96 px-4 rounded-md" src="https://i.ibb.co/3rhzz4M/IMG-20231209-080753-removebg-4-removebg-preview.png" alt="" />
                 </section>
             </div>
         </>
