f79655d485a36000b97bf202ba30d28c4624741e
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-25 19:09:35 +0600

    creates title for the contact page

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,7 +2,36 @@
 const Contacts = () => {
     return (
         <>
-         <h4>Contacts page here</h4>   
+            {/* section that will hold everything in this page */}
+
+            <section className="mt-5">
+
+                {/* this section if for the title of the section */}
+                <section>
+                    <div className="mb-20 text-center">
+                        <div className="w-11/12">
+                            <p className="mb-4 text-3l">Contact me</p>
+                            <p className="text-4xl font-semibold">Get in touch or start a project with me.</p>
+                        </div>
+                    </div>
+                </section>
+
+                {/* this section will hold the first part of the component links and image */}
+                <section>
+                    {/* social links and title  */}
+                    <section>
+
+                    </section>
+                    {/* image for the contact component */}
+                    <section>
+
+                    </section>
+                </section>
+                {/* this section will hold the form  */}
+                <section>
+
+                </section>
+            </section>
         </>
     );
 };
