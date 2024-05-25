f3e232d0a4270acbf917869d37a94052beacc17e
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-25 19:34:24 +0600

    added contacts page with image and the social linkd

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,3 +1,4 @@
+import { FaGithub, FaLinkedin, FaMedium, FaTwitter } from "react-icons/fa";
 
 const Contacts = () => {
     return (
@@ -11,20 +12,38 @@ const Contacts = () => {
                     <div className="mb-20 text-center">
                         <div className="w-11/12">
                             <p className="mb-4 text-3l">Contact me</p>
+
                             <p className="text-4xl font-semibold">Get in touch or start a project with me.</p>
                         </div>
                     </div>
                 </section>
 
                 {/* this section will hold the first part of the component links and image */}
-                <section>
+                <section className="md:flex md:justify-center md:gap-10 md:items-center">
                     {/* social links and title  */}
                     <section>
-
+                        <ul>
+                            <li className="flex justify-start items-center">
+                                <FaGithub />
+                                <span>Let&apos;s get connected with Github</span>
+                            </li>
+                            <li className="flex justify-start items-center">
+                                <FaTwitter />
+                                <span>follow me on Twitter</span>
+                            </li>
+                            <li className="flex justify-center items-center">
+                                <FaLinkedin />
+                                <span> Connect with me on Linkedin</span>
+                            </li>
+                            <li className="flex justify-center items-center">
+                                <FaMedium />
+                                <span>Stay connect on Medium</span>
+                            </li>
+                        </ul>
                     </section>
                     {/* image for the contact component */}
-                    <section>
-
+                    <section className="md:w-1/2">
+                        <img src="https://i.postimg.cc/8z3fYfgc/121369.jpg" alt="" />
                     </section>
                 </section>
                 {/* this section will hold the form  */}
