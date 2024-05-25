9d71687be9fcfd00acf88ca6d7bbd70718f84ef7
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-25 21:51:30 +0600

    created  a basic form

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,3 +1,4 @@
+import { FaGithub, FaLinkedin, FaMedium, FaTwitter } from "react-icons/fa";
 
 const Contacts = () => {
     return (
@@ -13,13 +14,41 @@ const Contacts = () => {
                         </div>
                     </div>
                 </section>
-                {/* section to hold the social links */}
-                <section>
-                    
-                </section>
-                {/* section to hold the form */}
-                <section>
+                {/* section to hold the links and the image */}
+                <section className="flex justify-center items-center">
+                    {/* section to hold the social links */}
+                    <section>
+                        <ul>
+                            <li className="flex justify-start items-center">
+                                <FaGithub />
+                                <span>
+                                    Let&apos;s get connected with Github
+                                </span>
+                            </li>
+                            <li className="flex justify-start items-center">
+                                <FaTwitter />
+                                Stay together at Twitter
+                            </li>
+                            <li className="flex justify-start items-center">
+                                <FaLinkedin />
+                                Connect me on Linkedin
+                            </li>
+                            <li className="flex justify-start items-center">
+                                <FaMedium />
+                                connect on medium.
+                            </li>
+                        </ul>
+                    </section>
+                    {/* section to hold the form */}
+                    <section>
+                        <form className="grid grid-cols-1 gap-5">
+                            <input type="text" placeholder="Name" />
+                            <input type="email" placeholder="Email" />
+                            <textarea name="Messege" placeholder="Messege" id="">
 
+                            </textarea>
+                        </form>
+                    </section>
                 </section>
             </section>
         </>
