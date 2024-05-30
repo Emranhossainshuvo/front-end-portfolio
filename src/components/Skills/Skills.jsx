e5bdcf426f70312e404fad9a258ee8848b16ecdf
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-30 12:26:25 +0600

    skills page all dommy texts replaced

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -29,20 +29,20 @@ const Skills = () => {
                     {/* first skills */}
                     <div className="bg-white hover:bg-cyan-800 delay-200 hover:text-white text-black  p-6 rounded-md shadow-lg">
 
-                        <div className="flex items-center justify-center mb-4">
+                        <div className="flex items-center justify-center mb-2">
                             <div className="w-16 h-16 bg-blue-100 shadow-blue-300 shadow-sm rounded-full flex items-center justify-center">
                                 <FaReact className="text-blue-600 text-3xl" />
                             </div>
                         </div>
                         <h4 className="text-xl font-bold text-center mb-2">ReactJS</h4>
                         <p className=" text-center">
-                             After learning the basics of React I start upgrading my ability in React.
+                            After learning the basics of React I start upgrading my ability in React.
                         </p>
                     </div>
                     {/* second skill  */}
                     <div className="bg-white hover:bg-cyan-800 delay-200 hover:text-white text-black  p-6 rounded-md shadow-lg">
 
-                        <div className="flex items-center justify-center mb-4">
+                        <div className="flex items-center justify-center mb-2">
                             <div className="w-16 h-16 bg-white shadow-black shadow-sm rounded-full flex items-center justify-center">
                                 <RiTailwindCssFill className="text-blue-600 text-3xl" />
                             </div>
@@ -55,7 +55,7 @@ const Skills = () => {
                     {/* third skill  */}
                     <div className="bg-white hover:bg-cyan-800 delay-200 hover:text-white text-black  p-6 rounded-md shadow-lg">
 
-                        <div className="flex items-center justify-center mb-4">
+                        <div className="flex items-center justify-center mb-2">
                             <div className="w-16 h-16 bg-[#C6E9BC] shadow-[#C6E9BC] shadow-sm rounded-full flex items-center justify-center">
                                 <FaNode className="text-blue-600 text-3xl" />
                             </div>
@@ -68,7 +68,7 @@ const Skills = () => {
                     {/* fourth skill  */}
                     <div className="bg-white hover:bg-cyan-800 delay-200 hover:text-white text-black  p-6 rounded-md shadow-lg">
 
-                        <div className="flex items-center justify-center mb-4">
+                        <div className="flex items-center justify-center mb-2">
                             <div className="w-16 h-16 bg-[#F1C617] shadow-[#F1C617] shadow rounded-full flex items-center justify-center">
                                 <SiExpress className="text-black text-3xl" />
                             </div>
@@ -79,55 +79,55 @@ const Skills = () => {
                         </p>
                     </div>
                     {/* fifth skill */}
-                    <div className="bg-white hover:bg-cyan-800 delay-200 hover:text-white text-black  p-6 rounded-md shadow-lg">
+                    <div className="bg-white hover:bg-cyan-800 delay-200 hover:text-white text-black  py-6 rounded-md shadow-lg">
 
-                        <div className="flex items-center justify-center mb-4">
+                        <div className="flex items-center justify-center mb-2">
                             <div className="w-16 h-16 bg-[#0B9EE6] shadow-[#0B9EE6] shadow rounded-full flex items-center justify-center">
                                 <SiFirebase className="text-[#FFCE35] text-3xl" />
                             </div>
                         </div>
                         <h4 className="text-xl font-bold text-center mb-2">Firebase</h4>
-                        <p className="  text-center">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, nesciunt.
+                        <p className="text-center">
+                            I am well-versed in Firebase, a comprehensive app development  and authentication platform by Google.
                         </p>
                     </div>
                     {/* sixth skill */}
                     <div className="bg-white hover:bg-cyan-800 delay-200 hover:text-white text-black  p-6 rounded-md shadow-lg">
 
-                        <div className="flex items-center justify-center mb-4">
+                        <div className="flex items-center justify-center mb-2">
                             <div className="w-16 h-16 bg-white shadow-black shadow rounded-full flex items-center justify-center">
                                 <DiGit className="text-[#F05539] text-3xl" />
                             </div>
                         </div>
                         <h4 className="text-xl font-bold text-center mb-2">Git</h4>
                         <p className="  text-center">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, nesciunt.
+                            I am proficient in Git, a version control system that enables me to track changes in source code.
                         </p>
                     </div>
                     {/* seventh skill */}
                     <div className="bg-white hover:bg-cyan-800 delay-200 hover:text-white text-black  p-6 rounded-md shadow-lg">
 
-                        <div className="flex items-center justify-center mb-4">
+                        <div className="flex items-center justify-center mb-2">
                             <div className="w-16 h-16 bg-black shadow-black shadow rounded-full flex items-center justify-center">
                                 <SiJsonwebtokens className=" text-white text-3xl" />
                             </div>
                         </div>
                         <h4 className="text-xl font-bold text-center mb-2">JWT - JSON Web Token</h4>
                         <p className="  text-center">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, nesciunt.
+                            I have basic expertise in JWT, a self-contained way for transmitting information between parties securely.
                         </p>
                     </div>
                     {/* eighth skill */}
                     <div className="bg-white hover:bg-cyan-800 delay-200 hover:text-white text-black  p-6 rounded-md shadow-lg">
 
-                        <div className="flex items-center justify-center mb-4">
+                        <div className="flex items-center justify-center mb-2">
                             <div className="w-16 h-16 bg-[#001E2B] shadow-[#001E2B] shadow rounded-full flex items-center justify-center">
                                 <BiLogoMongodb className="text-[#00ED64] text-3xl" />
                             </div>
                         </div>
                         <h4 className="text-xl font-bold text-center mb-2">Mongodb</h4>
                         <p className="  text-center">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, nesciunt.
+                            I am proficient in MongoDB, it allows me to store and manage large sets of unstructured data with flexibility and scalability.
                         </p>
                     </div>
                 </div>
