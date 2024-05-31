3d33422c20c376c0f0a7fdb3ca4312f1360bc896
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-31 09:27:01 +0600

    removed aos from navbar and the banner section

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import 'aos/dist/aos.css';
 import { FaBars, FaTimes } from "react-icons/fa";
-import Aos from "aos";
 
 const Navbar = () => {
 
@@ -11,22 +10,16 @@ const Navbar = () => {
         setMenuOpen(!menuOpen)
     };
 
-
-    useEffect(() => {
-        Aos.init();
-    });
-
-
     return (
         <>
             {/* main div to hold everything in the navbar file */}
             <div className="flex justify-between pt-5 items-center">
                 {/* div for left side element such as name and icon */}
-                <div data-aos="fade-right">
+                <div>
                     <h3 className="text-2xl font-semibold"><span className="text-[#FF0642]">  Emran</span> Hossain</h3>
                 </div>
 
-                <div data-aos="fade-left" className={`menu-links absolute md:static top-0 right-5 md:flex ${menuOpen ? 'block' : 'hidden'} text-lg font-medium`}>
+                <div className={`menu-links absolute md:static top-0 right-5 md:flex ${menuOpen ? 'block' : 'hidden'} text-lg font-medium`}>
                     <ul className="md:flex items-center gap-5">
                         <li className="hover:text-[#FF0642]">About</li>
                         <li className="hover:text-[#FF0642]">Projects</li>
